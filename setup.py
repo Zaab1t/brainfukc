a40b9df1cb92d40bd00d2df2b9a3504a7b8487bb
Carl Bordum Hansen <carl@bordum.dk>
2017-07-02 16:43:45 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='brainfukc',
+    description='Inline brainfuck for Python',
+    url='https://github.com/Zaab1t/brainfukc',
+    author='Carl Bordum Hansen',
+    license='MIT',
+    version='0.0.1',
+    py_modules=['brainfukc']
+)
